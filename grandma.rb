9d188dd1561e226217a64fc2c654a6390a647124
Andrew Shupe <adshupe@ucdavis.edu>
2018-11-28 01:46:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ if speak_to_grandma == "whats good g".downcase
   puts "HUH?! SPEAK UP, SONNY"
 elsif speak_to_grandma == "whats good g".upcase
   puts "NO, NOT SINCE 1938!"
-else speak_to_grandma == "I LOVE YOU GRANDMA!"
+else speak_to_grandma == "i love you grandma!".upcase 
   puts "I LOVE YOU TOO PUMPKIN!"
 end
 
