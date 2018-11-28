29481247ba3f84525e167501425272e7a6aafe57
Andrew Shupe <adshupe@ucdavis.edu>
2018-11-28 01:31:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 speak_to_grandma = "HUH?! SPEAK UP, SONNY"
 
-if speak_to_grandma == .
-# Whatever you say to grandma, she should respond with
+if speak_to_grandma == (.upcase) 
+  a, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
